a9fae4179a38fd213c79690348cf5bead91d23f9
skillsensi-web <skillsensi@gmail.com>
2026-06-10 01:06:54 +0530

    Add files via upload
    temporary

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int sum, num1, num2;
+    cout << "Enter number 1 : ";
+    cin >> num1;
+    cout << "Enter number 2 : ";
+    cin >> num2;
+    cout << "Sum of num1 and num2 is " << num1 + num2;
+    return 0;
+}
